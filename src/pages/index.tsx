dfaf162b1e8dd9dfd02df1557cd1fe719d7a55d2
Syed Ali Uz Zaman <45460148+alyaxi@users.noreply.github.com>
2021-06-14 03:27:53 +0500

    added logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,12 @@ import * as React from "react"
 import Bookmark from "../components/Bookmark"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "../components/Bookmark.css"
+import logo from "../assets/bookmark.png"
 
 const IndexPage = () => (
  <div className="main-container">
-   <h1 className="heading">My Bookmark Application</h1>
+   <h1 className="heading"><img src={logo} style={{width: "50px"}}  className="img-fluid img-thumbnail" alt="let" />My Bookmark Application</h1>
+   
    <Bookmark />
  </div>
 )
