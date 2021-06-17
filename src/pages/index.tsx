1bc1f5f256f522d4631ef9fae62a5727620d50f2
Syed Ali Uz Zaman <45460148+alyaxi@users.noreply.github.com>
2021-06-17 19:45:26 +0500

    added dependencies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import Bookmark from "../components/Bookmark"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "../components/Bookmark.css"
-import logo from "../assets/bookmark.png"
+import logo from "../components/assets/bookmark.png"
 
 const IndexPage = () => (
  <div className="main-container">
